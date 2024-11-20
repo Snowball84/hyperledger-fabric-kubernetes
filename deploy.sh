cf5325066fbe838f3125bc73ff1108c2c2c9de36
Snowball <1957955857@qq.com>
2024-11-20 21:13:00 +0800

    modified

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ kubectl apply -f k8s/namespaces
 kubectl apply -f k8s/secrets/org1.dummy.com
 kubectl apply -f k8s/secrets/org2.dummy.com
 kubectl apply -f k8s/secrets/org3.dummy.com
+kubectl apply -f k8s/storage/sc
 kubectl apply -f k8s/storage/pv
 kubectl apply -f k8s/storage/pvc
 kubectl apply -f k8s/job
-kubectl wait --timeout=120s --for=condition=complete --namespace dummy-com job.batch/setup
+kubectl wait --timeout=3600s --for=condition=complete --namespace dummy-com job.batch/setup
 kubectl delete -f k8s/job
 kubectl apply -k k8s
 kubectl wait --timeout=180s --for=condition=Ready --namespace dummy-com pods --all
