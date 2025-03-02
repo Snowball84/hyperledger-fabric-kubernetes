87c46fe724cfbc467e1c27b5fbdfc398581624aa
Snowball <1957955857@qq.com>
2025-03-03 01:43:32 +0800

    6peer per org

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl delete statefulset,deployment,ingress,service,job,secrets,pvc --all --namespace dummy-com && kubectl delete pv local-volume && kubectl delete pv local-volume-2 && kubectl delete pv local-volume-3 && kubectl delete pv local-volume-4
+kubectl delete statefulset,deployment,ingress,service,job,secrets,pvc --all --namespace dummy-com && kubectl delete pv local-volume && kubectl delete pv local-volume-2 && kubectl delete pv local-volume-3 && kubectl delete pv local-volume-4 && kubectl delete pv local-volume-5
 sudo rm -rf /mnt/nfs-share/*
--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 kubectl rollout restart daemonset -n kube-flannel kube-flannel-ds
 kubectl rollout restart deployment coredns -n kube-system
+kubectl -n ingress-nginx rollout restart deployment ingress-nginx-controller
